2bc1b54f6028efb89189252db697b36a2306bb87
WahyuSattriana <wahyusattriana@gmail.com>
2025-06-25 08:36:35 +0800

    feat: google login server-side complete

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -18,14 +18,12 @@ module.exports = class UserController {
       });
       const payload = ticket.getPayload();
 
-      // Find email in database
       let user = await User.findOne({
         where: {
           email: payload.email
         }
       });
 
-      // If not found, create a new user (name, email, password: randomly generated)
       if (!user) {
         user = await User.create({
           name: payload.name,
@@ -35,10 +33,7 @@ module.exports = class UserController {
         });
       }
 
-      // signToken using jwt with payload id user
       const access_token = signToken({ id: user.id });
-
-      // return / respond with access_token
       res.status(200).json({ message: `Welcome, ${user.name}`, access_token });
     } catch (error) {
       console.log(error);
